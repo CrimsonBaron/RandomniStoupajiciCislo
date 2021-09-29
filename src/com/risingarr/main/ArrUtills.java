13114a70543412f5d9b23c1c7bdcd7fb13872902
TheCrimsonBaron <68446060+TheCrimsonBaron@users.noreply.github.com>
2021-09-29 19:23:14 +0200

    second commit

--- src/com/risingarr/main/ArrUtills.java
+++ src/com/risingarr/main/ArrUtills.java
@@ -6,6 +6,18 @@ public class ArrUtills {
     }
 
     public static int[] randomRisingNumber(int length, int min, int max) {
+
+        ///--------------------------------------------------------
+        /// /* testy vysly takto: 10 50 100
+        ///                     ->53 60 63 64 73 75 81 84 87 99
+        ///                     ->53 58 58 64 64 78 84 89 94 96
+        ///                     ->51 54 64 72 73 74 75 95 98 98
+        ///                       10 25 100
+        ///                     ->26 37 45 53 55 63 71 82 90 98
+        ///                     ->31 35 38 44 55 67 77 85 85 98
+        ///                     ->30 37 39 41 45 51 57 58 79 96
+        ///--------------------------------------------------------
+
         int[] arr = new int[length];
         int size, h_max, h_min = min;
         size = Math.round((max - min) / length);
--- src/com/risingarr/main/Main.java
+++ src/com/risingarr/main/Main.java
@@ -2,7 +2,7 @@ package com.risingarr.main;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = ArrUtills.randomRisingNumber(10,50,100);
+        int[] arr = ArrUtills.randomRisingNumber(10,25,100);
         for (int a: arr
              ) {
             System.out.print(a+" ");
